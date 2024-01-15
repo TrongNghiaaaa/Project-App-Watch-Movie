7436ebc52515096494948e4a2612cf54073f7a42
Văn Nghĩa <nghiazorovn2304@gmail.com>
2024-01-15 10:56:45 +0700

    add lottie

--- lib/presentation/screens/splash_screen/splash_screen.dart
+++ lib/presentation/screens/splash_screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -15,17 +16,12 @@ class SplashScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("abnc"),
-            SvgPicture.asset(
-              "assets/images/Frame.svg",
-              height: 100,
-              width: 100,
+            Lottie.asset(
+              'assets/images/Animation - 1705290275566.json',
+              width: 200,
+              height: 200,
+              fit: BoxFit.fill,
             ),
-            SvgPicture.asset(
-              "assets/images/Star.svg",
-              height: 100,
-              width: 100,
-            )
           ],
         ),
       )),
